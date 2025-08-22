b516f037d8db60409b439e179c8f92ed460b9eb9
Saqibworksehsan <saqibworksehsan@gmail.com>
2025-08-22 06:31:41 +0530

    pages setup with ci/cd

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
+// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/trumaxgarments/', // ⬅️ must match repo name
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
